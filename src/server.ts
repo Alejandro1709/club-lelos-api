4afd6ffd203f9450273f09543b20e3753ac89e59
Alejandro LR <alejandro.lr.developer@hotmail.com>
2026-01-12 16:39:15 -0500

    Add: Connectdb mongodb

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import express, { type Application } from 'express'
 import dotenv from 'dotenv'
 import morgan from 'morgan'
+import connectDB from './config/db'
 
 dotenv.config()
 
 const app: Application = express()
+
+connectDB()
 
 app.use(express.json())
 
